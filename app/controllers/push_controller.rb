eaa417f242fc17fa7d67cb44151f423b0787873d
David House <davidahouse@gmail.com>
2012-09-17 07:55:06 -0400

    added in redis insert

--- app/controllers/push_controller.rb
+++ app/controllers/push_controller.rb
@@ -8,7 +8,9 @@ class PushController < ApplicationController
 		log.save!
 
 		# also save this to redis so any client can pick it up
-
+		uri = URI.parse(ENV["REDISTOGO_URL"])
+		REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+		REDIS.lpush("incoming",params[:payload])
 	end
 
 	def index
