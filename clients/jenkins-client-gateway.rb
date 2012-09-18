06346e9938c46343abd5c206d5497da45e55fb32
David House <davidahouse@gmail.com>
2012-09-18 10:12:55 -0400

    removed comment

--- clients/jenkins-client-gateway.rb
+++ clients/jenkins-client-gateway.rb
@@ -5,7 +5,6 @@ require 'pp'
 require 'net/http'
 
 puts "connecting to redis..."
-#uri = URI.parse("redis://redistogo:826b08473244ef80d3c11447f946a7c3@fish.redistogo.com:9203/")
 uri = URI.parse(ENV['JCG_REDIS_URI'])
 r = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
